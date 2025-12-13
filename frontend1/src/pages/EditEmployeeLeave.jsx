e80922cfb2c33ca6a304703aa5e8e1c3dfc06bc7
Deepu <deekir5010@gmail.com>
2025-12-13 16:51:48 +0530

    Update EditEmployeeLeave.jsx

--- frontend1/src/pages/EditEmployeeLeave.jsx
+++ frontend1/src/pages/EditEmployeeLeave.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/authentication'; // ✅ FIXED
+import { useAuth } from "../context/AuthContext"; // ✅ FIXED
 import { API_BASE } from '../config';
 
 export default function EditEmployeeLeave() {
