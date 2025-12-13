b3e5646ecdddd7e531c5b5b46e8fc931786fcb77
Deepu <deekir5010@gmail.com>
2025-12-13 16:50:52 +0530

    Update App.jsx

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider, useAuth } from "./context/authentication"; // ✅ FIXED
+import { AuthProvider, useAuth } from "./context/AuthContext";// ✅ FIXED
 
 // Pages
 import Login from "./pages/Login";
